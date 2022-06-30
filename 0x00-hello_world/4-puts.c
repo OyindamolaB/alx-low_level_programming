dc8dc9997bbb3d26d1d64e6c0a8cf477eca1af1f
Oyindamola Banjoko <oyinbanjoko@outlook.com>
2022-06-30 16:57:22 -0700

    Printing words

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 #include <string.h>
 /**
-<<<<<<< HEAD
- * * main-Entry point
- * * Return: Always 0 (Success)
- */
- int main (void)
- {
+* * main-Entry point
+* * Return: Always 0 (Success)
+*/
+int main (void)
+{
      puts("\"Programming is like building a multilingual puzzle");
      return (0);
-  }
-=======
- * * main - Entry point
- * * Return: Always 0 (Success)
- */
-int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
 }
->>>>>>> 34e8c91cd10b7b02f91eb98c22b5338f73d5e910
