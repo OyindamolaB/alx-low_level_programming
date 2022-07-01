540b10ea7d77a9dca59de030bef474e47c40ced6
Oyindamola Banjoko <oyinbanjoko@outlook.com>
2022-07-02 00:06:11 +0100

    Printing all possible combinations of three digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,12 +5,13 @@
 *
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 int digit1, digit2;
 for (digit1 = 0; digit1 < 9;  digit1++)
 {
-for (digit2 = digit1 + 1; digit2 < 100; digit2++)
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
