1ede4e224a714b4039a62a8a753c7730c316f2e4
Oyindamola Banjoko <oyinbanjoko@outlook.com>
2022-07-02 00:11:19 +0100

    Printing all possible combinations of three digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,12 +15,12 @@ for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
-
 if (digit1 == 8 && digit2 == 9)
 continue;
 
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 
