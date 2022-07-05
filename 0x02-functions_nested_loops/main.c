83b8d6dd5c5c85bfc85510d63d1d770d01b26fea
Oyindamola Banjoko <oyinbanjoko@outlook.com>
2022-07-05 22:00:07 +0100

    Checking lowercase character

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -5,17 +5,8 @@
 *
 * Return: Always 0.
 */
-int main(void)
+int main(void);
 {
-
-int r;
-
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
+print_alphabet_x10();
 return (0);
 }
