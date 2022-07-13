01ad1651dfe14f7a42ce971578c88a108005dbe9
Oyindamola Banjoko <oyinbanjoko@outlook.com>
2022-07-13 13:56:43 +0100

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,11 @@
 
 void print_rev(char *s)
 {
-int i = 0;
-while (s[i] != 0)
-{
-i++;
-}
-while (i > 0)
-{
-i--;
-_putchar(s[i]);
-}
+int len = 0, index = 0;
+len = _strlen(s);
+
+for (index = len - 1; index >= 0; index--)
+_putchar(s[index]);
+
 _putchar('\n')
 }
