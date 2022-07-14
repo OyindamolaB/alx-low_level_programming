f6eecc6d2f2f48fcc606ebf15b3f5d9e3944e41d
OyindamolaB <105212612+OyindamolaB@users.noreply.github.com>
2022-07-15 00:00:21 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,4 +14,19 @@ while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 
 if (str[index - 1] == ' ' ||
-
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}')
+str[index] -= 32;
+}
+return (str);
+}
