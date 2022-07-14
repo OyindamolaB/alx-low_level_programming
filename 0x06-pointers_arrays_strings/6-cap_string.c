50f18555950a409fc3bf1e07d13cb2f4b046f25a
OyindamolaB <105212612+OyindamolaB@users.noreply.github.com>
2022-07-15 00:03:28 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
 * cap_string - capitalize first character of a word
 * @str: string to capitalize
-* Return: returns capitalized string
+* Return: returns the capitalized string
 */
 
 char *cap_string(char *str)
 {
 int index = 0;
+
 while (str[++index])
 {
 while (!(str[index] >= 'a' && str[index] <= 'z'))
