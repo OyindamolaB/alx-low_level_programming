f21dce4295f390e9b270ce059f07186213c93231
OyindamolaB <105212612+OyindamolaB@users.noreply.github.com>
2022-07-19 16:32:35 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,16 +9,24 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-int i, j;
-for (i = 0; haystac[i] != '\0'; i++)
-{
-for (j = 0; needle[j] != '\0'; j++)
-{
-if (haystack[i + j] != needle[j])
-break;
-}
-if (!needle[j])
-return (&haystack[i]);
-}
-return (NULL);
+unsigned int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
 }
