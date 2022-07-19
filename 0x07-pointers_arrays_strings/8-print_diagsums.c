33194cf9f9534b552455e9f17ad720d91f321a98
OyindamolaB <105212612+OyindamolaB@users.noreply.github.com>
2022-07-19 17:15:22 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,23 +1,24 @@
 #include "main.h"
 /**
-* print_diagsums - print the sum of two diagonals
-* @a: pointer to start of matrix
-* @size: width of matric column
-*
-* Return: void
-*/
+ * print_diagsums - prints the sums of the two diagonals 
+ * @a: pointer to start of matrix
+ * @size: width of matrix column
+ *
+ * Return: void
+ */
 void print_diagsums(int *a, int size)
 {
-int i, j, p, 1  = 0, r = 0;
-for (i = 0; i < size; i++)
-{
-p = (i * size) + i;
-1 += *(a + p);
-}
-for (j = 0; j < size; j++)
-{
-p = (j * size) + (size - 1 - j);
-r += *(a + p);
-}
-printf("%i, %i\n", 1, r);
+	int i, j, p, l = 0, r = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - 1 - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
 }
