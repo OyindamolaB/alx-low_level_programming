e67c182d2423db7b958a19bb9a4ee70309b2575c
OyindamolaB <105212612+OyindamolaB@users.noreply.github.com>
2022-07-27 02:35:45 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - prints the number of arguments
- * @args: number of arguments
- * @argv: array of arguments
- * Return: the number of arguments
- */
+* main - prints the number of arguments
+* @argc: number of arguments
+* @argv:array of arguments
+* Return: the number of arguments
+*/
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 printf("%d\n", argc - 1);
