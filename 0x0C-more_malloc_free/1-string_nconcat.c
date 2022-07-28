eb1b3319e833889ef244ee5c89067d59cf19ccd9
Oyindamola Banjoko <oyinbanjoko@outlook.com>
2022-07-28 15:26:28 -0700

    Concatenates two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+* string_nconcat - function that concatenates two strings
+* @s1: first char
+* @s2: second char
+* @n: unsigned int
+* Return: if the function fails, return NULL
+*/
