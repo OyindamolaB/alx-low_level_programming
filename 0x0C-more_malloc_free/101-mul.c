eec2a1fde3ba73fdd8c90857446905fc4fa4b0ae
Oyindamola Banjoko <oyinbanjoko@outlook.com>
2022-07-28 16:06:37 -0700

    Multiplying two numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* mul - multiplies a char with a string
+* @n: char to multiply
+* @num: string to multiply
+* @num_index: last non NULL index of num
+* @dest: destination of multiplication
+* @dest_index: highest index to start addition
+* Return: pointer to dest, or NULL on failure
+*/
