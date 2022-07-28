a2b9fbd76bd5edfff06f5f1628e2cf2bf2ba5419
Oyindamola Banjoko <oyinbanjoko@outlook.com>
2022-07-28 15:36:33 -0700

    Allocating memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+* _calloc - allocates memory for an array
+* @size: allocates element of size to bytes
+* @nmemb: allocate memory for array
+* Return: pointer to allocated memory
+*/
