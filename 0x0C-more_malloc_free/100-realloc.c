9ba7a6ba6fb87c0615ea1835c16d6a5f28910c72
Oyindamola Banjoko <oyinbanjoko@outlook.com>
2022-07-28 15:55:01 -0700

    Reallocating memory block using malloc and free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+* _realloc - reallocates memory using malloc and free
+* @ptr: pointer to previously allocated memory
+* @old_size: size of allocated space for ptr in bytes
+* @new_size: new size of the memory block in bytes
+* Return: pointer allocate new size memory, or NULL
+*/
