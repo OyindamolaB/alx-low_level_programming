9cc17d40affbd42f31aebe15dd970bbb34ea9d54
OyindamolaB <105212612+OyindamolaB@users.noreply.github.com>
2022-07-28 23:38:15 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,3 +6,16 @@
 * @nmemb: allocate memory for array
 * Return: pointer to allocated memory
 */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *a;
+unsigned int b;
+if (nmemb == 0 || size == 0)
+return (NULL);
+a = malloc(nmemb * size);
+if (a == NULL)
+return (NULL);
+for (b = 0; b < (nmemb * size); b++)
+a[b] = 0;
+return (a);
+}
