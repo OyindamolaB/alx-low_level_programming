7ebc4ea4acfeea480dffabff6468b325bbf808bc
Oyindamola Banjoko <oyinbanjoko@outlook.com>
2022-08-16 08:56:29 -0700

    Singly Linked List

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* _r - reallocates memory for an array of pointers
+* to the nodes in a linked list
+* @list: the old list to append
+* @size: size of the new list (always one more than the old list)
+* @new: new node to add to the list
+* Return: pointer to the new list
+*/
