39b97645a677b6b6f94801fab522fe83f7dac298
Oyindamola Banjoko <oyinbanjoko@outlook.com>
2022-08-24 03:41:53 -0700

    Copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* check97 - checks for the correct number of arguments
+* @argc: number of arguments
+* Return: void
+*/
+
+void check97(int argc)
